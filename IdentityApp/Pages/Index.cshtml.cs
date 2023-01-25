ab4e93ad908b8291eb94333a8e8dad2d1a46237e
Sheroz Puladov <sherozpulatov3@gmail.com>
2023-01-25 21:27:02 +0500

    Secrets Management

--- IdentityApp/Pages/Index.cshtml.cs
+++ IdentityApp/Pages/Index.cshtml.cs
@@ -1,20 +1,80 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IdentityApp.Data;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace IdentityApp.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        public Dictionary<string, int> revenueSubmitted;
+        public Dictionary<string, int> revenueApproved;
+        public Dictionary<string, int> revenueRejected;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        private readonly ILogger<IndexModel> _logger;
+        private readonly ApplicationDbContext _context;
+        public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public void OnGet()
         {
+            InitDict(ref revenueSubmitted);
+            InitDict(ref revenueApproved);
+            InitDict(ref revenueRejected);
 
+            var invoices = _context.Invoice.ToList();
+
+            foreach(var invoice in invoices)
+            {
+                try
+                {
+                    switch (invoice.Status)
+                    {
+                        case InvoiceStatus.Submitted:
+                            revenueSubmitted[invoice.InvoiceMonth] += (int)invoice.InvoiceAmount;
+                            break;
+
+                        case InvoiceStatus.Approve:
+                            revenueApproved[invoice.InvoiceMonth] += (int)invoice.InvoiceAmount;
+                            break;
+
+                        case InvoiceStatus.Reject:
+                            revenueRejected[invoice.InvoiceMonth] += (int)invoice.InvoiceAmount;
+                            break;
+
+                        default:
+                            break;
+                    }
+
+                    
+                }
+                catch(Exception ex)
+                {
+                    _logger.LogError(ex.Message);
+                    continue;                }
+                
+            }
+        }
+
+        private void InitDict(ref Dictionary<string, int> dict)
+        {
+            dict = new Dictionary<string, int>()
+            {
+                { "January", 0 },
+                { "February", 0 },
+                { "March", 0 },
+                { "April", 0 },
+                { "May", 0 },
+                { "June", 0 },
+                { "July", 0 },
+                { "August", 0 },
+                { "September", 0 },
+                { "October", 0 },
+                { "November", 0 },
+                { "December", 0 }
+            };
         }
     }
 }
